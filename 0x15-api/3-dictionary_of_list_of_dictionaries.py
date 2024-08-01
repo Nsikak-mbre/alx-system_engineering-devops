f892e44c44f9d0a6ebded29f9e7245a86ae128bf
Nsikak-mbre <tm260715@gmail.com>
2024-08-01 01:04:46 +0100

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
 """
-using REST API to gather todo list progress, and
+Using REST API to gather todo list progress, and
 exports the data in JSON format.
 """
 
 import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    # fetch user data
+    # Fetch user data
     users_response = requests.get('https://jsonplaceholder.typicode.com/users')
     users = users_response.json()
 
-    # create dictionary to hold data
+    # Create dictionary to hold data
     todo_data = {}
 
-    # loop through each user
+    # Loop through each user
     for user in users:
         user_id = user['id']
         username = user['username']
 
-    # Fetch todo for the user
-    todo_response = requests.get(
-        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
-    todos = todo_response.json()
+        # Fetch todo for the user
+        todo_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+        todos = todo_response.json()
 
-    tasks = []
+        tasks = []
 
-    for todo in todos:
-        task_info = {
-            "username": username,
-            "task": todo['title'],
-            "completed": todo['completed']
-        }
-        tasks.append(task_info)
+        for todo in todos:
+            task_info = {
+                "username": username,
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+            tasks.append(task_info)
 
-    todo_data[user_id] = tasks
+        todo_data[user_id] = tasks
+
+    # Write the data to a JSON file
     with open('todo_all_employees.json', 'w') as json_file:
         json.dump(todo_data, json_file)
