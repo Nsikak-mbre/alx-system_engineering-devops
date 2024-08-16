b7124c92c4c19c679146a5dcdcef24bd2192e4a3
Nsikak-mbre <tm260715@gmail.com>
2024-08-16 11:32:25 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""
-This module provides a function to query the Reddit API
-and return the number of subscribers for a given subreddit
-"""
+"""Returns number of subscribers for a given reddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers
-    for a given subreddit. If the subreddit is invalid, returns 0.
+    """Queries the Reddit API and returns the number of
+        subscribers for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit to query.
@@ -17,14 +13,14 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': '0-subs/number_of_subscribers'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; RedditAPI/1.0)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return data['data']['subscribers']
+            return data.get("data", {}).get("subscribers", 0)
         else:
             return 0
     except requests.RequestException:
