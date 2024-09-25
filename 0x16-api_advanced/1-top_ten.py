27ded65d2b63258c863faef46b99ec3553fc7c75
Nsikak-mbre <tm260715@gmail.com>
2024-09-25 14:31:03 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Querries the redit API.
+Queries the Reddit API.
 """
 import requests
 
 
 def top_ten(subreddit):
     """
-    Returns the 10 hot posts from reddit api querries.
+    Prints the titles of the 10 hot posts from a Reddit API query.
 
     Args:
         subreddit (str): The name of the subreddit to query.
@@ -19,13 +19,24 @@ def top_ten(subreddit):
     headers = {'User-Agent': '1-top_ten/top_ten_posts'}
 
     try:
+        # Make GET request to Reddit API
         response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if the response was successful (status code 200)
         if response.status_code == 200:
             data = response.json()
             posts = data.get('data', {}).get('children', [])
-            for post in posts:
-                print(post['data']['title'])
+            
+            # Check if there are any posts
+            if posts:
+                # Print the titles of the posts
+                for post in posts:
+                    print(post['data']['title'])
             else:
                 print(None)
+        else:
+            # If subreddit is invalid or doesn't exist
+            print(None)
     except requests.RequestException:
+        # In case of a request failure
         print(None)
